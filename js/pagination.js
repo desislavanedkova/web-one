ba67e97d3a3ef3f53a1d7d60425a6ceab3f6fd0d
Десислава Венелинова Недкова <desi@teciztok.net>
2017-08-07 14:27:32 +0300

    paggination fixed

--- js/pagination.js
+++ js/pagination.js
@@ -1,3 +1,25 @@
-function pagination(ulType, direction) {
+function addPageNumber(ulType) {
+  var pageId = '#' + ulType + '-page';
 
+  $(pageId).append(1);
+}
+
+function addPagesCouner(ulType, count) {
+  var pagesId = '#' + ulType + '-pages',
+      liCount = count;
+
+  $(pagesId).append(Math.round(liCount/3));
+}
+
+function pagination(ulType) {
+  var ulElements = '#' + ulType + "-elements",
+      liCount = $(ulElements).children().length;
+
+  addPageNumber(ulType);
+  addPagesCouner(ulType, liCount);
+
+  //hide arrows if number of pages is 1
+  if (liCount/3 == 1) {
+    $(ulElements).siblings('.arrows').children('*').css('visibility', 'hidden');
+  }
 }
