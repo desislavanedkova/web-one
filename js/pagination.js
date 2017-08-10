097f170d768e8bf1b15f6e28738cd0d1093a096c
Десислава Венелинова Недкова <desi@teciztok.net>
2017-08-10 16:21:37 +0300

    4etvyrtak

--- js/pagination.js
+++ js/pagination.js
@@ -1,18 +1,18 @@
 function addPagesCouner(ulType, count) {
-  var pagesId = '#' + ulType + '-pages',
+  var $pagesId = $('#' + ulType + '-pages'),
       liCount = count;
 
-  $(pagesId).append(Math.round(liCount/3));
+  $pagesId.append(Math.round(liCount/3));
 }
 
 function pagination(ulType) {
-  var ulElements = '#' + ulType + "-elements",
-      liCount = $(ulElements).children().length;
+  var $ulElements = $('#' + ulType + "-elements"),
+      liCount = $ulElements.children().length;
 
   addPagesCouner(ulType, liCount);
 
   //hide arrows if number of pages is 1
   if (liCount/3 == 1) {
-    $(ulElements).siblings('.arrows').children('*').css('visibility', 'hidden');
+    $ulElements.siblings('.arrows').children('*').css('visibility', 'hidden');
   }
 }
